579eae0f1d10a82de87fcc3ee1a1c2839a822c57
Jose Fco de la Fuente <josefcodelafuente@gmail.com>
2024-11-20 14:25:54 +0100

    feat: mod zone

--- setup.sh
+++ setup.sh
@@ -22,21 +22,23 @@ gcloud services enable container.googleapis.com \
     cloudbuild.googleapis.com
 
 # Create a Google Artifact Registry called my-repository in the specified region
-# gcloud artifacts repositories create my-repository \
-#   --repository-format=docker \
-#   --location=$REGION
+gcloud artifacts repositories create my-repository \
+  --repository-format=docker \
+  --location=$REGION
 
 # Create a GKE Standard cluster called hello-cluster
 # It must be zonal, with 3 nodes
 # Min nodes=2, max nodes=6, and cluster autoscaler enabled
 # Release channel=Regular, and with cluster version specified
-# gcloud container clusters create hello-cluster \
-#   --zone $ZONE \
-#   --num-nodes=3 --min-nodes=2 --max-nodes=6 --enable-autoscaling \
-#   --release-channel=regular \
+gcloud container clusters create dashboard-sonar-cluster \
+  --zone $ZONE \
+  --num-nodes=3 --min-nodes=2 --max-nodes=6 --enable-autoscaling \
+  --release-channel=regular \
 
 # We can verify it
-# gcloud container clusters list
+gcloud container clusters list
 
 # we need to obtain the credentials for our cluster
-# gcloud container clusters get-credentials hello-cluster --zone $ZONE
+gcloud container clusters get-credentials dashboard-sonar-cluster --zone $ZONE
+
+
